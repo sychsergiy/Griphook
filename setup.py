d69b8e504450d421a85c1d41e83b2e40876bb9ad
Oleksandr Zinkevych <zso040399@gmail.com>
2018-07-11 14:26:25 +0300

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+NAME = 'griphook'
+DESCRIPTION = 'Server monitoring and billing service'
+URL = 'http://git.interns.evo/EVO2018-Vlad-team/Griphook/'
+AUTHOR = 'Vlad Team'
+REQUIRED_PYTHON = '>=3.6.0'
+VERSION = '1.0.0'
+
+REQUIRED_PACKAGES = [
+    'requests', 'celery', 'sqlalchemy'
+]
+
+setup(
+    name=NAME,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    python_requires=REQUIRED_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED_PACKAGES,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
