30913dada4cfbfa77a9340aa9ff383291045e8c7
d.pavlushchenko <pavlushchenkod@gmail.com>
2018-07-11 15:30:00 +0300

    fixed cantal formatter

--- griphook/api/formatters.py
+++ griphook/api/formatters.py
@@ -1,6 +1,6 @@
-from typing import NamedTuple
-from json import loads
+from typing import NamedTuple, Union
 from re import findall
+import json
 
 
 class Metric(NamedTuple):
@@ -10,16 +10,21 @@ class Metric(NamedTuple):
     service: str
 
 
-def format_cantal_data(data: str) -> list:
-    series_data = loads(data)
+def format_cantal_data(data: str) -> Union[list, None]:
+    try:
+        series_data = json.loads(data)
+    except json.decoder.JSONDecodeError:
+        return None
 
     formatted_metrics = []
     for serie in series_data:
         target = findall(r'\.([\w:-]+)', serie['target'])
-        metric = Metric(value=serie['datapoints'][0][0],
-                        type=target[6],
-                        services_group=target[4].split(':')[0],
-                        service=target[4].split(':')[1]
-                        )
-        formatted_metrics.append(metric)
+
+        if serie['datapoints']:
+            metric = Metric(value=serie['datapoints'][0][0],
+                            type=target[6],
+                            services_group=target[4].split(':')[0],
+                            service=target[4].split(':')[1]
+                            )
+            formatted_metrics.append(metric)
     return formatted_metrics
