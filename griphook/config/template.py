a58d8c4aff0fa57869a0609ea7ccbbdcce98f9d6
Sergiy <sychsergiy2@gmail.com>
2018-07-11 17:11:44 +0300

    update trafaret template and test

--- griphook/config/template.py
+++ griphook/config/template.py
@@ -1,19 +1,17 @@
 import trafaret
 
 template = trafaret.Dict({
-    'api': trafaret.String(),
-    'cli': trafaret.String(),
-    'db': trafaret.String(),
-    'tasks': trafaret.Dict({
+    "api": trafaret.Dict({
+        "GRAPHITE_URL": trafaret.String(),
+    }),
+    "db": trafaret.Dict({
+        "DATABASE_URL": trafaret.String(),
+    }),
+    "tasks": trafaret.Dict({
         "DATA_SOURCE_DATA_EXPIRES": trafaret.String(),
         "CELERY_BROKER_URL": trafaret.String(),
+        "BROKER_DATABASE_URL": trafaret.String(),
         "TRYING_SETUP_PARSER_INTERVAL": trafaret.Int(),
         "PARSE_METRIC_EXPIRES": trafaret.Int(),
     })
-
 })
-
-# DATABASE_URL
-# BROKER_DATABASE_URL
-
-# GRAPHITE_URL
--- griphook/config/test_config.py
+++ griphook/config/test_config.py
@@ -11,20 +11,36 @@ from config import Config, BASE_DIR
 class TestConfig(unittest.TestCase):
 
     def setUp(self):
-        self.FILE_NAME = BASE_DIR + '/config.yml'
+        self.FILE_NAME = BASE_DIR + "/config.yml"
 
         self.test_template = trafaret.Dict({
-            'api': trafaret.String(),
-            'cli': trafaret.String(),
-            'db': trafaret.String(),
-            'tasks': trafaret.String(),
+            "api": trafaret.String(),
+            "cli": trafaret.String(),
+            "db": trafaret.String(),
+            "tasks": trafaret.String(),
         })
+
+        self.current_config_data = {
+            "api": {
+                "GRAPHITE_URL": "url_here"
+            },
+            "db": {
+                "DATABASE_URL": "url here"
+            },
+            "tasks": {
+                "DATA_SOURCE_DATA_EXPIRES": "test",
+                "CELERY_BROKER_URL": "test",
+                "BROKER_DATABASE_URL": "url here",
+                "TRYING_SETUP_PARSER_INTERVAL": 1,
+                "PARSE_METRIC_EXPIRES": 1,
+            },
+        }
 
     def tearDown(self):
         os.remove(self.FILE_NAME)
 
     def write_yml_config(self, data):
-        with open(self.FILE_NAME, 'w') as file:
+        with open(self.FILE_NAME, "w") as file:
             yaml.dump(data, file, default_flow_style=False)
 
     def test_correct_config(self):
@@ -53,59 +69,29 @@ class TestConfig(unittest.TestCase):
                 Config(self.test_template)
 
     def test_current_config(self):
-        data = {
-            "api": "value",
-            "cli": "value",
-            "db": "value",
-            "tasks": {
-                "DATA_SOURCE_DATA_EXPIRES": "test",
-                "CELERY_BROKER_URL": "test",
-                "TRYING_SETUP_PARSER_INTERVAL": 1,
-                "PARSE_METRIC_EXPIRES": 1,
-            },
-        }
+        data = self.current_config_data
+
         self.write_yml_config(data)
 
         options = Config().options
         self.assertEqual(options, data)
 
-    def test_overwriting_options_from_env(self):
-        data = {
-            "api": "value",
-            "cli": "value",
-            "db": "value",
-            "tasks": {
-                "DATA_SOURCE_DATA_EXPIRES": "test",
-                "CELERY_BROKER_URL": "test",
-                "TRYING_SETUP_PARSER_INTERVAL": 1,
-                "PARSE_METRIC_EXPIRES": 1,
-            },
-        }
+    def test_overwriting_options_from_env_of_current_template(self):
+        data = self.current_config_data
 
         self.write_yml_config(data)
-        data['tasks']['DATA_SOURCE_DATA_EXPIRES'] = 'new_value'
+        data["tasks"]["DATA_SOURCE_DATA_EXPIRES"] = "new_value"
 
-        os.environ['GH_DATA_SOURCE_DATA_EXPIRES'] = "new_value"
+        os.environ["GH_DATA_SOURCE_DATA_EXPIRES"] = "new_value"
         config = Config()
 
         self.assertEqual(config.options, data)
 
     def test_overwriting_by_env_variable_with_wrong_type(self):
-        data = {
-            "api": "value",
-            "cli": "value",
-            "db": "value",
-            "tasks": {
-                "DATA_SOURCE_DATA_EXPIRES": "test",
-                "CELERY_BROKER_URL": "test",
-                "TRYING_SETUP_PARSER_INTERVAL": 1,
-                "PARSE_METRIC_EXPIRES": 1,
-            },
-        }
-
+        data = self.current_config_data
         self.write_yml_config(data)
 
-        os.environ['GH_PARSE_METRIC_EXPIRES'] = "string here"  # set wrong data type
+        os.environ["GH_PARSE_METRIC_EXPIRES"] = "string here"  # set wrong data type
 
         with self.assertRaises(SystemExit):
             with self.assertRaises(trafaret.DataError):
@@ -114,5 +100,5 @@ class TestConfig(unittest.TestCase):
         os.environ.pop("GH_PARSE_METRIC_EXPIRES", None)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
