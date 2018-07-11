d4dda6e892b96ee9d5e7b628038123476a6d2723
Oleksandr Zinkevych <zso040399@gmail.com>
2018-07-11 16:50:53 +0300

    Ignored InsecureRequestWarning

--- griphook/api/tests/test_graphite_parser.py
+++ griphook/api/tests/test_graphite_parser.py
@@ -1,7 +1,13 @@
 import datetime
 import json
 
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
 from griphook.api.parsers import GraphiteAPIParser
+
+# To ignore insecure https connection
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 def test_fetch_returns_json():
@@ -9,6 +15,8 @@ def test_fetch_returns_json():
 
     time_until = int(datetime.datetime.now().timestamp())
     time_from = int(time_until - datetime.timedelta(hours=1).total_seconds())
+
     data = parser.fetch(time_from=time_from, time_until=time_until)
+
     # Check if returns data
     json.loads(data)
