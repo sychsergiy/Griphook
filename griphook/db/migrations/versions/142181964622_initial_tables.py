8737139a268c2d3fc2f1b0d351b43fcb614c8a7c
Sych <sychsergiy2@gmail.com>
2018-07-12 17:17:16 +0300

    init alembic, add initial migration

---
+++ griphook/db/migrations/versions/142181964622_initial_tables.py
@@ -0,0 +1,24 @@
+"""initial_tables
+
+Revision ID: 142181964622
+Revises: 
+Create Date: 2018-07-12 17:15:20.428018
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '142181964622'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
