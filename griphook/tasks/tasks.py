737452ec8dd80d12b880816c1df8bea357b63e17
Oleksandr Kravchuk <lnalkman@gmail.com>
2018-07-12 14:21:35 +0300

    Added parse_metric task realization for cantal metric and test config

--- griphook/tasks/tasks.py
+++ griphook/tasks/tasks.py
@@ -1,19 +1,34 @@
 import os
+from datetime import datetime
 
 from celery import Celery
 from celery import Task
 from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 
-import celeryconfig as conf
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import desc
+from sqlalchemy import select
+
+from griphook.config.config import Config
+from griphook.api.data_source import DataSource
+from griphook.api import parsers, formatters
+from griphook.db.models import Metric
 
 
-app = Celery(broker_url=os.environ.get('CELERY_BROKER_URL'))
+conf = Config().options
+app = Celery(broker_url=conf['tasks']['CELERY_BROKER_URL'])
 logger = get_task_logger(__name__)
+
+engine = create_engine(conf['db']['DATABASE_URL'])
+if not engine.dialect.has_table(engine, Metric.__tablename__):
+    Metric.__table__.create(engine)
+Session = sessionmaker(bind=engine)
 
 
 @periodic_task(
-    run_every=conf.TRYING_SETUP_PARSER_INTERVAL,
+    run_every=conf['tasks']['TRYING_SETUP_PARSER_INTERVAL'],
     name='start_parser',
     ignore_result=True
 )
@@ -28,7 +43,7 @@ def start_parser():
 class ParsingTask(Task):
 
     def on_failure(self, exc, task_id, args, kwargs, einfo):
-        logger.warning('Parsing metrics failed')
+        logger.exception('Parsing metrics failed')
 
     def on_retry(self, exc, task_id, args, kwargs, einfo):
         logger.retry('Retry parse metrics')
@@ -37,17 +52,39 @@ class ParsingTask(Task):
 @app.task(base=ParsingTask)
 def parse_metrics():
     """
+    Parse data from Cantal API and save it to specified in Session db.
+    This task should be executed only by one worker at the same time.
     :TODO
-        1. Calculate `from` and `until` timestamps using last metric in DB and timedelta between
-           metrics.
-           If there is no metric in db, set `from` to
-           datetime.datetime.now() - DATA_SOURCE_DATA_EXPIRES,
-           where DATA_SOURCE_DATA_EXPIRES -> how long data in data source is saved
-
-        2. Using calculated `from` and `until` timestamps - get metric.
-
-        3. Save metric in DB
-
-        4. If `until` + `timedelta between metrics` < datetime.datetime.now() -> Start task again
+        Change task to parse data from different API's
     """
-    pass
+    session = Session()
+    cantal_source = DataSource(
+        parser=parsers.GraphiteAPIParser(base_url=conf['api']['GRAPHITE_URL']),
+        data_formatter=formatters.format_cantal_data,
+    )
+
+    # Get row with last timestamp in db and calculate time_from and time_until
+    row = session.execute(
+        select([Metric.time_from]).order_by(desc(Metric.time_from)).limit(1)
+    ).first()
+    if row is not None:
+        time_from = int(row.time_from.timestamp()) + conf['tasks']['DATA_GRANULATION']
+    else:
+        rounded_now = datetime.now().replace(microsecond=0,second=0,minute=0)
+        time_from = int(rounded_now.timestamp()) - conf['tasks']['DATA_SOURCE_DATA_EXPIRES']
+    time_until = time_from + conf['tasks']['DATA_GRANULATION']
+
+    if time_until <= datetime.now().timestamp():
+        logger.info('Getting data from api (time_from={}; time_until={})'.format(time_from, time_until))
+        data = cantal_source.read(time_from=time_from, time_until=time_until)
+        if data:
+            # Generate db Metric objects and save to db
+            session.add_all([
+                Metric(**metric._asdict(), time_from=datetime.fromtimestamp(time_from)) for metric in data
+            ])
+            session.commit()
+            logger.info('Saved {} metrics'.format(len(data)))
+        else:
+            logger.info('Got no data from api')
+
+        parse_metrics.delay()
