62d02098dc305e86dcdecb1c1b72ff4cefc7fdb6
Oleksandr Zinkevych <zso040399@gmail.com>
2018-07-18 21:08:01 +0300

    Tests for MultipleValues class

---
+++ griphook/api/graphite/tests/test_multiple_values.py
@@ -0,0 +1,19 @@
+import pytest
+
+from griphook.api.graphite.target import MultipleValues
+
+
+def test_multiple_value_works():
+    values = MultipleValues(values=['foo', 'bar', 'spam'])
+    assert str(values) == '{foo,bar,spam}'
+
+
+def test_non_strings():
+    values = MultipleValues(values=[1, 2, 3, True, 1.54])
+    assert str(values) == '{1,2,3,True,1.54}'
+
+
+def test_empty_values():
+    with pytest.raises(ValueError):
+        values = MultipleValues(values=[])
+        str(values)
