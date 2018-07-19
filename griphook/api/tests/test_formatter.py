367f088ce1a92746f17505c663256835544d0b3f
d.pavlushchenko <pavlushchenkod@gmail.com>
2018-07-19 18:36:49 +0300

    added test formatting function

--- griphook/api/tests/test_formatter.py
+++ griphook/api/tests/test_formatter.py
@@ -1,11 +1,18 @@
+import json
 import os
 import pathlib
-import json
-from griphook.api.formatters import validate_input_cantal_data, format_cantal_data, Metric
 
+from griphook.api.formatters import (Metric, format_cantal_data,
+                                     validate_input_cantal_data)
 
 BASE_DIR = pathlib.Path(__file__).parents[0]
 TEST_DATA_PATH = os.path.join(BASE_DIR, "test_input_data.json")
+
+# test data for check fields metric object
+LAST_METRIC_SERVICES_GROUP = 'galdr--backend'
+LAST_METRIC_SERVICE = 'ua'
+LAST_METRIC_VALUE = 573423616
+LAST_METRIC_TYPE = 'user_cpu_percent'
 
 
 class TestCantalFormatter:
@@ -18,14 +25,34 @@ class TestCantalFormatter:
         for data_series_object in valid_data:
             # check validated data filter
             assert data_series_object is not None
+
             # check data types validator
             assert isinstance(data_series_object.datapoints, list)
             assert isinstance(data_series_object.datapoints[0], tuple)
             assert isinstance(data_series_object.datapoints[0][0], float)
             assert data_series_object.datapoints[0][0] is not None
+
             # check metric target validator
             assert 'cantal' in data_series_object.target
             assert 'lithos' in data_series_object.target
+
+    def test_format_cantal_data(self):
+        test_data = get_test_data()
+        metrics = format_cantal_data(test_data)
+
+        # check fields last metric object
+        *_, last_metric = metrics
+        assert last_metric.services_group == LAST_METRIC_SERVICES_GROUP
+        assert last_metric.service == LAST_METRIC_SERVICE
+        assert last_metric.value == LAST_METRIC_VALUE
+        assert last_metric.type == LAST_METRIC_TYPE
+
+        # check type metric objects
+        for metric in metrics:
+            assert isinstance(metric, Metric)
+
+    def test_format_cantal_data_fail(self):  # TODO: check result for fail data
+        pass
 
 
 def get_test_data():
