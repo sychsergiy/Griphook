c9dd76566e4fcea4b752d5abede476d2e8fc029a
Sych <sychsergiy2@gmail.com>
2018-07-23 16:08:34 +0300

    minor fixes managers

--- griphook/cli/managers/project.py
+++ griphook/cli/managers/project.py
@@ -1,3 +1,5 @@
+from sqlalchemy.sql import exists
+
 from griphook.cli.managers.base import BaseManager
 from griphook.cli.managers.exceptions import ProjectManagerException
 from griphook.db.models import Project, ServicesGroup
@@ -5,20 +7,18 @@ from griphook.db.models import Project, ServicesGroup
 
 class ProjectManager(BaseManager):
     def create(self, title) -> None:
-        project = self.session.query(Project).filter_by(title=title).first()
-        if project:
+        if self.session.query(exists().where(Project.title == title)).scalar():
             raise ProjectManagerException('Project with the same name already exists')
 
-        instance = Project(title=title)
-        self.session.add(instance)
+        self.session.add(Project(title=title))
         self.session.commit()
 
     def attach_to_service_group(self, service_group_title: str, project_title: str) -> None:
-        project = self.session.query(Project).filter_by(title=project_title).first()
+        project = self.session.query(Project).filter_by(title=project_title).scalar()
         if not project:
             raise ProjectManagerException('Project with title {} doesn\'t exists'.format(project_title))
 
-        service_group = self.session.query(ServicesGroup).filter_by(title=service_group_title).first()
+        service_group = self.session.query(ServicesGroup).filter_by(title=service_group_title).scalar()
         if not service_group:
             raise ProjectManagerException('ServiceGroup with title {} doesn\'t exists'.format(service_group_title))
 
