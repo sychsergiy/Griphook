d945253ad992d8c4c82700ef9367c4126bfebcc4
Vlad <v.stetsenko@gmail.com>
2018-07-31 17:18:27 +0300

    fixed peaks query

--- griphook/server/peaks/utils.py
+++ griphook/server/peaks/utils.py
@@ -6,4 +6,4 @@ def round_time(since, until, step):
     m = datetime.timedelta(seconds=delta.total_seconds()) % datetime.timedelta(seconds=step)
     if m:
         until += datetime.timedelta(seconds=step) - m
-    return until + datetime.timedelta(days=1)
+    return until
--- griphook/server/peaks/views.py
+++ griphook/server/peaks/views.py
@@ -14,22 +14,27 @@ def get_peacks():
         step = int(request.args.get('step'))
         since = datetime.datetime.strptime(request.args.get('since'), '%Y-%m-%d')
         until = datetime.datetime.strptime(request.args.get('until'), '%Y-%m-%d')
-        service_group = request.args.get('service_group')
+        service_group = request.args.get('services_group')
         service = request.args.get('service')
     except ValueError:
-        abort(422)
+        abort(400)
 
     metric_type = request.args.get('metric_type')
     server = request.args.get('server')
     if not metric_type or not server:
-        abort(422)
+        abort(400)
 
     until = round_time(since, until, step=step)
+    since_timestamp = since.replace(tzinfo=datetime.timezone.utc).timestamp()
+    remainder = (since_timestamp % step)
+    group_time = (func.floor((func.extract('epoch', BatchStory.time) - remainder) / step)) * step
     query = (
         Metric.query
             .with_entities(
-            func.max(Metric.value).label("peaks"),
-            ((func.floor(func.extract('epoch', BatchStory.time) / step)) * step).label("step"))
+                func.max(Metric.value).label("peaks"),
+                func.min(func.extract('epoch', BatchStory.time)),
+                group_time.label('step')
+        )
             .group_by('step')
             .order_by('step')
             .join(BatchStory)
@@ -44,10 +49,16 @@ def get_peacks():
             query = query.filter(ServicesGroup.title==service_group)
             if service:
                 query = query.filter(Service.title)
+    print(query)
 
-    formatter = lambda x : (x[0], datetime.datetime.fromtimestamp(x[1], tz=pytz.utc).strftime('%Y-%m-%d %H:%M:%S'))
+    formatter = lambda x : (
+                x[0],
+                datetime.datetime.fromtimestamp(
+                    x[1],
+                    tz=pytz.utc
+                ).strftime('%Y-%m-%d %H:%M:%S')
+    )
     data = [formatter(element) for element in query.all()]
-
     response_data = {'data': data}
     response = current_app.response_class(
         response=json.dumps(response_data),
