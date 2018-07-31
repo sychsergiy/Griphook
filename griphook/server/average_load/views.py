4f46955554c52088a67d3b959f6491ecdabcd1e3
Sych <sychsergiy2@gmail.com>
2018-07-31 14:27:40 +0300

    server_average_load_api_view working now

--- griphook/server/average_load/views.py
+++ griphook/server/average_load/views.py
@@ -1,17 +1,13 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask.views import MethodView
 
 from griphook.server.average_load.graphite import (
     get_average_service_load_chart_data,
     get_average_services_group_load_chart_data,
-    get_average_server_load_chart_data
+    get_server_load_chart_data
 )
 from griphook.server.models import ServicesGroup, Service
 from griphook.server.average_load.mixins import QueryParametersForMethodMixin
-
-
-# todo: how graphite handle so many instances in query_params,
-#  check if it is possible to send only server or services_group
 
 
 class AbstractAverageLoadAPIView(QueryParametersForMethodMixin, MethodView):
@@ -22,9 +18,6 @@ class AbstractAverageLoadAPIView(QueryParametersForMethodMixin, MethodView):
         self.get_required_parameters = self.get_required_parameters + self._default_get_parameters
         super(AbstractAverageLoadAPIView, self).__init__(*args, **kwargs)
 
-    def services_query(self, **kwargs):
-        raise NotImplementedError
-
     def get(self):
         raise NotImplementedError
 
@@ -33,20 +26,10 @@ class ServerAverageLoadView(AbstractAverageLoadAPIView):
     get_required_parameters = ('server',)
 
     def get(self):
-        target_services = [':'.join(target) for target in self.services_query(self.parameters['server'])]
-        response_data = get_average_server_load_chart_data(self.parameters['server'], target_services,
-                                                           self.parameters['time_from'], self.parameters['time_until'])
-
-        response = jsonify(response_data)
-        return response
-
-    def services_query(self, server_title: str) -> list:
-        return (
-            Service.query
-                .filter(Service.server == server_title)
-                .join(ServicesGroup)
-                .with_entities(Service.server, ServicesGroup.title, Service.title, Service.instance)
-        ).all()
+        response_data = get_server_load_chart_data(
+            self.parameters['server'], self.parameters['time_from'],
+            self.parameters['time_until'], self.parameters['metric_type'])
+        return jsonify(response_data)
 
 
 class ServicesGroupAverageLoadView(AbstractAverageLoadAPIView):
@@ -88,7 +71,6 @@ class ServiceAverageLoadView(AbstractAverageLoadAPIView):
                 .with_entities(Service.server, ServicesGroup.title, Service.title, Service.instance)
         ).all()
 
-# todo: use summarize to get average for every instance,
-# todo: than use averageSeries graphite function to find total average
+# todo: average_load_server_view already working, code the same for service_groups and services
 
-# todo: change arguemtns (server_title, service_group_title, ...) to (server, services_group, ...) in filters blueprint
+# todo: change arguments (server_title, service_group_title, ...) to (server, services_group, ...) in filters blueprint
