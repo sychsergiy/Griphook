4c4b1c548285e935844be62dfd3f1af8c0dcbcb1
Sych <sychsergiy2@gmail.com>
2018-08-02 18:25:27 +0300

    refactor average load

--- griphook/server/average_load/helper.py
+++ griphook/server/average_load/helper.py
@@ -1,6 +1,8 @@
 import json
 import requests
 
+from typing import Union
+
 from griphook.api.graphite.target import DotPath
 from griphook.server.average_load.graphite import average, summarize
 from griphook.server.models import ServicesGroup, Service
@@ -14,57 +16,50 @@ def construct_target(metric_type, server='*', services_group='*', service='*', i
 class ChartDataHelper(object):
     def __init__(self, parent: str, metric_type: str):
         self.parent: str = parent
-        self.children: tuple = self.get_children()
+        self.children: tuple = self.retrieve_children()
         self.metric_type: str = metric_type
 
-    def get_children(self):
+    def retrieve_children(self):
         # todo graphite-avg function can't receive empty list
         raise NotImplementedError
 
-    def children_target_getter(self, children_item):
+    def children_target_constructor(self, children_item):
         raise NotImplementedError
 
-    def parent_target_getter(self):
+    def root_target_constructor(self):
         raise NotImplementedError
 
-    def parent_target(self):
-        target = self.parent_target_getter()
+    def root_target(self):
+        target = self.root_target_constructor()
         return average(summarize(target, "3month", "avg"))
 
     def children_target(self):
         # construct query with multiple targets
         for item in self.children:
-            target = self.children_target_getter(item)
+            target = self.children_target_constructor(item)
             yield average(summarize(target, "3month", 'avg'))
 
     def get_data(self, time_from, time_until):
-        parent_target = self.parent_target()
-
+        root_target = self.root_target()
         children_target = tuple(self.children_target())
 
-        helper = GraphiteHelper(parent_target, children_target, time_from, time_until)
-
-        parent_response = helper.send_request()
-        children_response = helper.send_request(for_children=True)
+        parent_response = send_request(root_target, time_from, time_until)
+        children_response = send_request(children_target, time_from, time_until)
 
         # parse json
-        parent_response_value = parent_response[0]['datapoints'][0][0]
+        root_response_value = parent_response[0]['datapoints'][0][0]
+        children_response_value = [{
+            'target': self.children_target_constructor(self.children[index]),
+            'values': value['datapoints'][0][0],
+        } for index, value in enumerate(children_response)]
 
-        def response_children_generator():
-            for index, value in enumerate(children_response):
-                # graphite returns seriesLists in the same order like targets was given
-                # so it is possible just to take service_group_title from services_groups_list with the same index
-                children_item = self.children[index]
-                yield {
-                    'target': self.children_target_getter(children_item),
-                    'value': value['datapoints'][0][0],
-                }
+        root_target_to_visualize = self.root_target_constructor()
 
-        children_response_value = list(response_children_generator())
-
-        parent_target_to_visualize = self.parent_target_getter()
-        result = helper.construct_response(parent_target_to_visualize, parent_response_value, children_response_value)
-        return result
+        response = {
+            'root': {'target': root_target_to_visualize, 'value': root_response_value},
+            'children': children_response_value
+        }
+        return response
 
 
 def services_group_target_getter(metric_type, parent, children_item):
@@ -72,28 +67,30 @@ def services_group_target_getter(metric_type, parent, children_item):
 
 
 class ServerChartDataHelper(ChartDataHelper):
-    def get_children(self) -> tuple:
+    def __init__(self, *args, **kwargs):
+        super(ServerChartDataHelper, self).__init__(*args, **kwargs)
+
+    def retrieve_children(self) -> tuple:
         services_groups = (
             Service.query
                 .filter(Service.server == self.parent)
                 .join(ServicesGroup).distinct()
                 .with_entities(ServicesGroup.title)
         ).all()
-        # convert to simple tuple with titles
         return tuple(services_group_title for (services_group_title,) in services_groups)
 
-    def children_target_getter(self, children_item) -> str:
+    def children_target_constructor(self, children_item) -> str:
         # get average value for each service_group inside this server
         # as service_group can be in few server, calculate only using instances from current server
-        # be careful, when you watch average for service_group it will be not the same
+        # be careful, when you watch average on service_group detail it will be not the same
         return construct_target(self.metric_type, server=self.parent, services_group=children_item)
 
-    def parent_target_getter(self) -> str:
+    def root_target_constructor(self) -> str:
         return construct_target(self.metric_type, server=self.parent)
 
 
 class ServicesGroupChartDataHelper(ChartDataHelper):
-    def get_children(self) -> tuple:
+    def retrieve_children(self) -> tuple:
         services = (
             ServicesGroup.query
                 .filter(ServicesGroup.title == self.parent)
@@ -105,60 +102,40 @@ class ServicesGroupChartDataHelper(ChartDataHelper):
         services = tuple(title for (title,) in services)
         return services
 
-    def children_target_getter(self, children_item) -> str:
+    def children_target_constructor(self, children_item) -> str:
         return construct_target(self.metric_type, services_group=self.parent, service=children_item)
 
-    def parent_target_getter(self) -> str:
+    def root_target_constructor(self) -> str:
         return construct_target(self.metric_type, services_group=self.parent)
 
 
 class ServicesChartDataHelper(ChartDataHelper):
-    def get_children(self) -> tuple:
+    def retrieve_children(self) -> tuple:
         services = (
             Service.query
                 .filter(Service.title == self.parent).distinct()
-                .with_entities(Service.instance)
+                .join(ServicesGroup)
+                .with_entities(Service.server, ServicesGroup.title, Service.title, Service.instance, )
         ).all()
-        return tuple(instance for (instance,) in services)
+        return services
 
-    def children_target_getter(self, children_item) -> str:
-        # todo: this part is wrong, services group can't be all,
-        # todo: services in different services groups are different services
-        return construct_target(self.metric_type, service=self.parent, instance=children_item)
+    def children_target_constructor(self, children_item) -> str:
+        server, group, service, instance = children_item
+        return construct_target(self.metric_type, server=server, services_group=group, service=service,
+                                instance=instance)
 
-    def parent_target_getter(self) -> str:
+    def root_target_constructor(self) -> str:
         return construct_target(self.metric_type, service=self.parent)
 
 
-class GraphiteHelper(object):
-    def __init__(self, parent_target: str, children_target: tuple, time_from: int, time_until: int):
-        self.parent_target: tuple = parent_target
-        self.children_target = children_target
-
-        self.time_from = time_from
-        self.time_until = time_until
-
-    def send_request(self, for_children: bool = False) -> dict:
-        base_url = 'https://graphite.olympus.evo/render'
-        target = self.children_target if for_children else self.parent_target
-        params = {
-            'format': 'json',
-            'target': target,
-            'from': str(self.time_from),
-            'until': str(self.time_until),
-        }
-        # todo: handle connection exception
-        response = requests.get(url=base_url, params=params or {}, verify=False)
-        return json.loads(response.text)
-
-    @staticmethod
-    def construct_response(target_to_visualize: str, parent_value: int, children_values: list) -> dict:
-        root_part = {
-            'target': target_to_visualize,
-            'value': parent_value
-        }
-        result = {
-            'root': root_part,
-            'children': children_values
-        }
-        return result
+def send_request(target: Union[str, tuple], time_from: int, time_until: int) -> dict:
+    base_url = 'https://graphite.olympus.evo/render'
+    params = {
+        'format': 'json',
+        'target': target,
+        'from': str(time_from),
+        'until': str(time_until),
+    }
+    # todo: handle connection exception
+    response = requests.get(url=base_url, params=params or {}, verify=False)
+    return json.loads(response.text)
