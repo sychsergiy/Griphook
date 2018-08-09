70fb086d294c515ad9ad065890ccf87e67a76abf
Vlad <v.stetsenko@gmail.com>
2018-08-09 17:27:03 +0300

    fixed peaks view

--- griphook/server/peaks/test_peaks.py
+++ griphook/server/peaks/test_peaks.py
@@ -5,7 +5,7 @@ from flask import url_for
 
 from griphook.server import db
 from griphook.tests.base import BaseTestCase
-from griphook.server.models import Metric, BatchStory, Service, ServicesGroup
+from griphook.server.models import MetricPeak, BatchStoryPeaks, Service, ServicesGroup, Server
 
 
 class BaseWithDBSession(BaseTestCase):
@@ -13,50 +13,57 @@ class BaseWithDBSession(BaseTestCase):
     def setUp(self):
         super(BaseWithDBSession, self).setUp()
         self.session = db.session
-        self.app = self.create_app()
-        self.client = self.app.test_client()
+        self.data_time_format = '%Y-%m-%d %H'
 
         service_group1 = ServicesGroup(title="group1")
         service_group2 = ServicesGroup(title="group2")
         self.session.add_all([service_group1, service_group2])
-        service1 = Service(title="service1", server="test1", services_group=service_group1)
-        service2 = Service(title="service2", server="test1", services_group=service_group2)
-        service3 = Service(title="service3", server="test2", services_group=service_group2)
+        self.session.commit()
+        server1 = Server(title="test1")
+        server2 = Server(title="test2")
+        self.session.add_all([server1, server2])
+        self.session.commit()
+        service1 = Service(title="service1", instance='test', server_id=server1.id, services_group_id=service_group1.id)
+        service2 = Service(title="service2", instance='test', server_id=server1.id, services_group_id=service_group2.id)
+        service3 = Service(title="service3", instance='test', server_id=server2.id, services_group_id=service_group2.id)
         self.session.add_all([service1, service2, service3])
+        self.session.commit()
         self.time1 = datetime.now() - timedelta(days=8)
         self.time2 = datetime.now()
-        batches_story1 = BatchStory(time=self.time1)
-        batches_story2 = BatchStory(time=self.time2)
+        batches_story1 = BatchStoryPeaks(time=self.time1)
+        batches_story2 = BatchStoryPeaks(time=self.time2)
         self.session.add_all([batches_story1, batches_story2])
-        metric1 = Metric(
+        self.session.commit()
+        metric1 = MetricPeak(
             value=2,
-            batch=batches_story1,
-            service=service1,
-            services_group=service_group1,
+            batch_id=batches_story1.id,
+            service_id=service1.id,
+            services_group_id=service_group1.id,
             type='user_cpu_percent'
         )
-        metric2 = Metric(
+        metric2 = MetricPeak(
             value=2,
-            batch=batches_story2,
-            service=service2,
-            services_group=service_group2,
+            batch_id=batches_story2.id,
+            service_id=service2.id,
+            services_group_id=service_group2.id,
             type='user_cpu_percent'
         )
-        metric3 = Metric(
+        metric3 = MetricPeak(
             value=3,
-            batch=batches_story2,
-            service=service3,
-            services_group=service_group2,
+            batch_id=batches_story2.id,
+            service_id=service3.id,
+            services_group_id=service_group2.id,
             type='user_cpu_percent'
         )
-        metric4 = Metric(
+        metric4 = MetricPeak(
             value=4,
-            batch=batches_story1,
-            service=service3,
-            services_group=service_group2,
+            batch_id=batches_story1.id,
+            service_id=service3.id,
+            services_group_id=service_group2.id,
             type='user_cpu_percent'
         )
         self.session.add_all([metric1, metric2, metric3, metric4])
+        self.session.commit()
         self.week = 604800
 
     def test_server_peak_data_validation(self):
@@ -65,15 +72,15 @@ class BaseWithDBSession(BaseTestCase):
             'server': 'test1',
             'metric_type': "user_cpu_percent",
             'step': self.week,
-            "since": self.time1.strftime('%Y-%m-%d'),
-            "until": self.time2.strftime('%Y-%m-%d')
+            "since": self.time1.strftime(self.data_time_format),
+            "until": self.time2.strftime(self.data_time_format)
         }
 
         for key in data:
             response = self.request_without_ruquied_field(self.client, url, data, key)
             self.assertEqual(response.status_code, 400)
 
-        response = self.client.get(url, query_string=data, follow_redirects=True)
+        response = self.client.post(url, data=json.dumps(data), follow_redirects=True, content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_peaks_query(self):
@@ -82,37 +89,39 @@ class BaseWithDBSession(BaseTestCase):
             'server': 'test2',
             'metric_type': "user_cpu_percent",
             'step': self.week,
-            "since": self.time1.strftime('%Y-%m-%d'),
-            "until": self.time2.strftime('%Y-%m-%d')
+            "since": self.time1.strftime(self.data_time_format),
+            "until": self.time2.strftime(self.data_time_format)
         }
-        response = self.client.get(url, query_string=data, follow_redirects=True)
+        response = self.client.post(url, data=json.dumps(data), follow_redirects=True, content_type='application/json')
         resp_data = json.loads(response.data.decode('utf-8'))['data']
-        self.assertEqual(len(resp_data), 2)
-        self.assertEqual(resp_data[0][1], self.time1.strftime('%Y-%m-%d %H:%M:%S'))
-        self.assertEqual(resp_data[1][1], self.time2.strftime('%Y-%m-%d %H:%M:%S'))
-        self.assertEqual(resp_data[0][0], 4)
-        self.assertEqual(resp_data[1][0], 3)
+        self.assertEqual(len(resp_data['timeline']), 2)
+        self.assertEqual(resp_data['timeline'][0], self.time1.strftime(self.data_time_format))
+        self.assertEqual(resp_data['timeline'][1], self.time2.strftime(self.data_time_format))
+        self.assertEqual(resp_data['values'][0], 4)
+        self.assertEqual(resp_data['values'][1], 3)
+        self.assertEqual(resp_data['metric_type'], data['metric_type'])
 
         data['server'] = 'test1'
-        response = self.client.get(url, query_string=data, follow_redirects=True)
+        response = self.client.post(url, data=json.dumps(data), follow_redirects=True, content_type='application/json')
         resp_data = json.loads(response.data.decode('utf-8'))['data']
-        self.assertEqual(len(resp_data), 2)
-        self.assertEqual(resp_data[0][1], self.time1.strftime('%Y-%m-%d %H:%M:%S'))
-        self.assertEqual(resp_data[1][1], self.time2.strftime('%Y-%m-%d %H:%M:%S'))
-        self.assertEqual(resp_data[0][0], 2)
-        self.assertEqual(resp_data[1][0], 2)
+        self.assertEqual(len(resp_data['timeline']), 2)
+        self.assertEqual(resp_data['timeline'][0], self.time1.strftime(self.data_time_format))
+        self.assertEqual(resp_data['timeline'][1], self.time2.strftime(self.data_time_format))
+        self.assertEqual(resp_data['values'][0], 2)
+        self.assertEqual(resp_data['values'][1], 2)
+        self.assertEqual(resp_data['metric_type'], data['metric_type'])
 
         data['step'] = self.week * 4
-        response = self.client.get(url, query_string=data, follow_redirects=True)
+        response = self.client.post(url, data=json.dumps(data), follow_redirects=True, content_type='application/json')
         resp_data = json.loads(response.data.decode('utf-8'))['data']
-        self.assertEqual(len(resp_data), 1)
-        self.assertEqual(resp_data[0][1], self.time1.strftime('%Y-%m-%d %H:%M:%S'))
-
+        self.assertEqual(len(resp_data['timeline']), 1)
+        self.assertEqual(resp_data['timeline'][0], self.time1.strftime(self.data_time_format))
+        self.assertEqual(resp_data['metric_type'], data['metric_type'])
 
     @staticmethod
     def request_without_ruquied_field(client, url, data, field):
         value = data[field]
         data.pop(field)
-        response = client.get(url, query_string=data, follow_redirects=True)
+        response = client.post(url, data=json.dumps(data), follow_redirects=True, content_type='application/json')
         data[field] = value
         return response
--- griphook/server/peaks/utils.py
+++ griphook/server/peaks/utils.py
@@ -1,9 +1,161 @@
 import datetime
 
+from sqlalchemy import func
+from sqlalchemy.orm import Query
+
+from griphook.server.models import MetricPeak, BatchStoryPeaks, Service, ServicesGroup, Server
+from griphook.server import db
+
 
 def round_time(since, until, step):
     delta = until - since
-    m = datetime.timedelta(seconds=delta.total_seconds()) % datetime.timedelta(seconds=step)
-    if m:
-        until += datetime.timedelta(seconds=step) - m
+    modular = datetime.timedelta(seconds=delta.total_seconds()) % datetime.timedelta(seconds=step)
+    if modular:
+        until += datetime.timedelta(seconds=step) - modular
     return until
+
+
+class MetricPeakGroupFilter(object):
+    def __init__(self, session, query: Query = None):
+        self.session = session
+        self.query = query if query else self.session.query(MetricPeak)
+        self.service_group_joined = False
+        self.service_joined = False
+        self.server_joined = False
+        self.batch_story_joined = False
+
+    def __join_service(self):
+        if not self.service_joined:
+            self.query = self.query.join(Service)
+            self.service_joined = True
+
+    def __join_server(self):
+        if not self.server_joined:
+            if not self.service_joined:
+                self.__join_service()
+            self.query = self.query.join(Server)
+            self.service_joined = True
+
+    def __join_service_group(self):
+        if not self.service_group_joined:
+            if not self.service_joined:
+                self.__join_service()
+            self.query = self.query.join(ServicesGroup, MetricPeak.services_group_id == ServicesGroup.id)
+            self.service_group_joined = True
+
+    def __join_batch_story(self):
+        if not self.batch_story_joined:
+            self.query = self.query.join(BatchStoryPeaks)
+            self.batch_story_joined = True
+
+    def filter_by_server_title(self, *args: str):
+        self.__join_server()
+        self.query = self.query.filter(Server.title.in_(args))
+        return self
+
+    def filter_by_service_group_title(self, *args: str):
+        self.__join_service_group()
+        self.query = self.query.filter(ServicesGroup.title.in_(args))
+        return self
+
+    def filter_by_service_title(self, *args: str):
+        self.__join_service()
+        self.query = self.query.filter(Service.title.in_(args))
+        return self
+
+    def filter_by_time_period(self, since, until):
+        self.__join_batch_story()
+        self.query = self.query.filter(BatchStoryPeaks.time.between(since, until))
+        return self
+
+    def filter_by_metric_type(self, *args: str):
+        self.query = self.query.filter(MetricPeak.type.in_(args))
+        return self
+
+    def set_query_entities(self, *args):
+        self.query = self.query.with_entities(*args)
+        return self
+
+    def group_by(self, *args: str):
+        self.query = self.query.group_by(*args)
+        return self
+
+    def order_by(self, label: str):
+        self.query = self.query.order_by(label)
+        return self
+
+    def get_items(self):
+        return self.query.all()
+
+
+def get_shift(since, step):
+    since_timestamp = since.replace(tzinfo=datetime.timezone.utc).timestamp()
+    return since_timestamp % step
+
+
+def peaks_query(query_data):
+    server = query_data.get('server')
+    since = query_data.get('since')
+    until = query_data.get('until')
+    step = query_data.get('step')
+    metric_type = query_data.get('metric_type')
+    service = query_data.get('service')
+    service_group = query_data.get('service_group')
+    until = round_time(since, until, step=step)
+    shift = get_shift(since, step)
+    group_time = (func.floor((func.extract('epoch', BatchStoryPeaks.time) - shift) / step)) * step
+    query_creator = MetricPeakGroupFilter(session=db.session)
+    (
+        query_creator
+        .filter_by_metric_type(metric_type)
+        .filter_by_time_period(since=since, until=until)
+        .filter_by_server_title(server)
+        .set_query_entities(
+            func.max(MetricPeak.value).label("peaks"),
+            group_time.label('step'),
+            func.min(func.extract('epoch', BatchStoryPeaks.time)).label('time'),
+            MetricPeak.type.label('type')
+        )
+        .group_by('step', 'type')
+        .order_by('time')
+    )
+    if service_group:
+        query_creator.filter_by_service_group_title(service_group)
+        if service:
+            query_creator.filter_by_service_title(service)
+    return query_creator.query
+
+
+def peak_formatter(peak):
+    return (
+        datetime.datetime.fromtimestamp(
+            peak.time,
+            tz=datetime.timezone.utc
+        ).strftime('%Y-%m-%d %H')
+    )
+
+
+def validate_peaks_query(args):
+    date_time_format = '%Y-%m-%d %H'
+    data = dict()
+    error_data = dict()
+    try:
+        data['step'] = int(args.get('step'))
+    except TypeError:
+        error_data = {'Error': "Error step field is required"}
+    except ValueError:
+        error_data = {'Error': "Error step format. Expected int"}
+    try:
+        data['since'] = datetime.datetime.strptime(args.get('since'), date_time_format)
+        data['until'] = datetime.datetime.strptime(args.get('until'), date_time_format)
+    except TypeError:
+        error_data = {'Error': "since and until are required fields"}
+    except ValueError:
+        error_data = {'Error': "Error datetime (since or until) format. Expected {0}".format(date_time_format)}
+    data['service_group'] = args.get('services_group')
+    data['service'] = args.get('service')
+    data['metric_type'] = args.get('metric_type')
+    data['server'] = args.get('server')
+    if not data['metric_type'] or not data['server']:
+        error_data = {'Error': "metric_type and server are required fields"}
+    return data, error_data
