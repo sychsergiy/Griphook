2d69cedd00e83937fb66840eef0c72d191333863
d.pavlushchenko <pavlushchenkod@gmail.com>
2018-08-14 16:30:19 +0300

    added team endpoint

--- griphook/server/settings/route.py
+++ griphook/server/settings/route.py
@@ -5,7 +5,12 @@ from griphook.server.settings import views
 
 settings_blueprint = Blueprint('settings', __name__, )
 
+# routes for settings page
 settings_blueprint.add_url_rule('/', 'settings', view_func=views.index)
 settings_blueprint.add_url_rule('/general', 'general', view_func=views.index)
 
+# routes for settings API
 settings_blueprint.add_url_rule('/project', view_func=views.ProjectSettingsAPI.as_view('project'))
+settings_blueprint.add_url_rule('/team', view_func=views.TeamSettingsAPI.as_view('team'))
+# settings_blueprint.add_url_rule('/server', view_func=views.ServerSettingsAPI.as_view('server'))
+# settings_blueprint.add_url_rule('/cluster', view_func=views.ClusterSettingsAPI.as_view('cluster'))
--- griphook/server/settings/views.py
+++ griphook/server/settings/views.py
@@ -1,12 +1,19 @@
 import json
 
 from flask.views import MethodView
-from flask import request, abort, render_template, jsonify, Response
+from flask import request, render_template, jsonify
 
 from griphook.server import db
-from griphook.server.models import Project
+from griphook.server.models import Project, Team
 from griphook.server.managers.project_manager import ProjectManager
-from griphook.server.managers.exceptions import ProjectManagerException
+from griphook.server.managers.team_manager import TeamManager
+from griphook.server.managers.server_manager import ServerManager
+
+from griphook.server.managers.exceptions import (
+    ProjectManagerException,
+    TeamManagerException,
+    ServerManagerException
+)
 
 
 class ProjectSettingsAPI(MethodView):
@@ -15,29 +22,68 @@ class ProjectSettingsAPI(MethodView):
         try:
             new_project = ProjectManager(db.session).create(data.get('title'))
         except ProjectManagerException as exc:
-            abort(400, exc)
-
+            return jsonify({'error': exc.error_text}), 400
         return jsonify(
             {
+                'success': True,
                 'id': new_project.id,
                 'title': new_project.title
             }
-        )
+        ), 200
 
     def put(self):
-        pass
+        data = json.loads(request.data)
+        try:
+            ProjectManager(db.session).update(data.get('id'), data.get('title'))
+        except ProjectManagerException as exc:
+            return jsonify({'error': exc.error_text}), 400
+        return jsonify({'success': True}), 200
 
     def delete(self):
         data = json.loads(request.data)
         try:
             ProjectManager(db.session).delete(data.get('id'))
         except ProjectManagerException as exc:
-            abort(400, exc)
+            return jsonify({'error': exc.error_text}), 400
+        return jsonify({'success': True}), 200
 
-        return Response(status=200)
+
+class TeamSettingsAPI(MethodView):
+    def post(self):
+        data = json.loads(request.data)
+        try:
+            new_team = TeamManager(db.session).create(data.get('title'))
+        except TeamManagerException as exc:
+            return jsonify({'error': exc.error_text}), 400
+
+        return jsonify(
+            {
+                'success': True,
+                'id': new_team.id,
+                'title': new_team.title
+            }
+        ), 200
+
+    def put(self):
+        data = json.loads(request.data)
+        try:
+            TeamManager(db.session).update(data.get('id'), data.get('title'))
+        except TeamManagerException as exc:
+            return jsonify({'error': exc.error_text}), 400
+        return jsonify({'success': True}), 200
+
+    def delete(self):
+        data = json.loads(request.data)
+        try:
+            TeamManager(db.session).delete(data.get('id'))
+        except TeamManagerException as exc:
+            return jsonify({'error': exc.error_text}), 400
+        return jsonify({'success': True}), 200
+
 
 
 def index():
     session = db.session
     projects = session.query(Project).all()
-    return render_template('settings/general.html', projects=projects)
+    teams = session.query(Team).all()
+    return render_template('settings/general.html', projects=projects, teams=teams)
