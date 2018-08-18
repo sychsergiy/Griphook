e4ff84afaa5ea5291a5e79674aeca9da36c2f7fd
Sergiy Sych <sychsergiy2@gmail.com>
2018-08-18 13:08:59 +0300

    billing get_pie_chart_relative_data view added

--- griphook/server/billing/pie_chart_views.py
+++ griphook/server/billing/pie_chart_views.py
@@ -27,10 +27,12 @@ class GetPieChartAbsoluteDataView(View):
       }
       response format:
       {
-        "labels": [str],
-        "values": [int],
+        "labels": ["selected", "rest"],
+        "values": [int, int],
       ]
-
+    return metric sum of selected_part
+     (services groups filters by request params)
+      relatively to total metric sum
     """
 
     methods = ["POST"]
@@ -50,8 +52,8 @@ class GetPieChartAbsoluteDataView(View):
         elif target_type == "cluster":
             initial_query = (
                 Cluster.query.filter(Cluster.id.in_(target_ids))
-                    .join(Server)
-                    .join(Service)
+                .join(Server)
+                .join(Service)
             )
         elif target_type == "server":
             initial_query = Server.query.filter(Server.id.in_(target_ids)).join(Service)
@@ -59,25 +61,25 @@ class GetPieChartAbsoluteDataView(View):
             initial_query = ServicesGroup.query.filter(ServicesGroup.id.in_(target_ids))
         elif target_type == "team":
             initial_query = Team.query
+            # todo: need to check if it is working
+            # better to join MetricBilling explicit
         elif target_type == "project":
             initial_query = Project.query
         else:
             raise Exception("Problem with request data validation")
 
-        selected_metric_sum = self.get_query_metric_sum(
-            initial_query, request_data
-        )
+        selected_metric_sum = self.get_query_metric_sum(initial_query, request_data)
         # if target ids are not found and selected_metric_sum is None, just set 0
         # todo: maybe it will be better to return 404
         selected_metric_sum = selected_metric_sum if selected_metric_sum else 0
 
         response_data = {
-            "labels": ["selected", "rest"],
-            "values": [
+            "labels": ("selected", "rest"),
+            "values": (
                 # todo: solve problem with ceiling in big number
                 selected_metric_sum,
                 total_metric_sum - selected_metric_sum,
-            ],
+            ),
         }
         return jsonify(response_data)
 
@@ -87,15 +89,17 @@ class GetPieChartAbsoluteDataView(View):
          MetricBillling table, must be joined with Service or ServicesGroup
         """
         metric_sum = (
+            # todo: need to explicit join with Service or ServicesGroup
+            # to avoid unpredictable behavior
             initial_query.join(MetricBilling)
-                .join(BatchStoryBilling)
-                .filter(MetricBilling.type == request_data["metric_type"])
-                .filter(
+            .join(BatchStoryBilling)
+            .filter(MetricBilling.type == request_data["metric_type"])
+            .filter(
                 BatchStoryBilling.time.between(
                     request_data["time_from"], request_data["time_until"]
                 )
             )
-                .with_entities(func.sum(MetricBilling.value).label("metric_sum"))
+            .with_entities(func.sum(MetricBilling.value).label("metric_sum"))
         ).scalar()
         return metric_sum
 
@@ -115,32 +119,78 @@ class GetPieChartRelativeDataView(View):
         "labels": [str],
         "values": [int],
       ]
-
+    :return list of services groups and their's metric sum
+    services groups are filtered by request params
     """
 
     methods = ["POST"]
 
     def dispatch_request(self):
+        # todo: add request data validation
         request_data = request.get_json()
 
+        target_type = request_data["target_type"]
+        target_ids = request_data["target_ids"]
+
+        if target_type == "all":
+            initial_query = ServicesGroup.query
+        elif target_type == "cluster":
+            initial_query = (
+                Cluster.query.filter(Cluster.id.in_(target_ids))
+                .join(Server, Server.cluster_id == Cluster.id)
+                .join(Service, Service.server_id == Server.id)
+                .join(ServicesGroup, Service.services_group_id == ServicesGroup.id)
+            )
+        elif target_type == "server":
+            initial_query = (
+                Server.query.filter(Server.id.in_(target_ids))
+                .join(Service, Service.server_id == Server.id)
+                .join(ServicesGroup, ServicesGroup.id == Service.services_group_id)
+            )
+        elif target_type == "services_group":
+            initial_query = ServicesGroup.query.filter(ServicesGroup.id.in_(target_ids))
+        elif target_type == "project":
+            initial_query = Project.query.join(
+                ServicesGroup, Project.id == ServicesGroup.project_id
+            )
+        elif target_type == "team":
+            initial_query = Team.query.join(
+                ServicesGroup, Team.id == ServicesGroup.team_id
+            )
+        else:
+            raise Exception("Problem with request data validation, wrong target type")
+
+        groups_metric_sum_values = self.get_metric_sum_for_each_group(
+            initial_query, request_data
+        )
+
+        labels, values = zip(*groups_metric_sum_values)
+
+        response_data = {"labels": labels, "values": values}
+        return jsonify(response_data)
+
+    def get_metric_sum_for_each_group(self, initial_query, request_data):
+        """
+        :param initial_query: query of ServicesGroups
+        :param request_data: filter params
+        :return: metric sum for each services group in initial query
+        """
+
         total = (
-            ServicesGroup.query.join(Service)
-                .join(MetricBilling)
-                .join(BatchStoryBilling)
-                .filter(MetricBilling.type == request_data["metric_type"])
-                .filter(
+            initial_query.join(
+                MetricBilling, MetricBilling.services_group_id == ServicesGroup.id
+            )
+            .join(BatchStoryBilling)
+            .filter(MetricBilling.type == request_data["metric_type"])
+            .filter(
                 BatchStoryBilling.time.between(
                     request_data["time_from"], request_data["time_until"]
                 )
             )
-                .group_by(ServicesGroup.title)
-                .order_by(func.sum(MetricBilling.value).desc())
-                .with_entities(
+            .group_by(ServicesGroup.title)
+            .order_by(func.sum(MetricBilling.value).desc())
+            .with_entities(
                 ServicesGroup.title, func.sum(MetricBilling.value).label("metric_sum")
             )
-        )
-
-        print(total)
-
-        # if request_data['target_type'] == 'all':
-        #     passResource
+        ).all()
+        return total
--- griphook/server/billing/route.py
+++ griphook/server/billing/route.py
@@ -5,5 +5,11 @@ from . import pie_chart_views
 billing_blueprint = Blueprint("billing", __name__)
 
 billing_blueprint.add_url_rule(
-    "/get_pie_chart_data", view_func=pie_chart_views.GetPieChartAbsoluteDataView.as_view("get-pie-chart-data")
+    "/get_pie_chart_absolute_data",
+    view_func=pie_chart_views.GetPieChartAbsoluteDataView.as_view("get-pie-chart-absolute-data")
+)
+
+billing_blueprint.add_url_rule(
+    "/get_pie_chart_relative_data",
+    view_func=pie_chart_views.GetPieChartRelativeDataView.as_view("get-pie-chart-relative-data")
 )
