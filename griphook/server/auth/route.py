1c466b7add62bbd0f7b662222d53d39ca182f4d6
Oleksandr Zinkevych <zso040399@gmail.com>
2018-08-23 20:26:25 +0300

    Created LoginView

--- griphook/server/auth/route.py
+++ griphook/server/auth/route.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
-from griphook.server.auth.views import login
+from .views import LoginView
 
 auth_blueprint = Blueprint("auth", __name__)
 
-auth_blueprint.add_url_rule("/login", "login", login)
+auth_blueprint.add_url_rule("/login", "login", view_func=LoginView.as_view("login"))
