b552f7464393af8815033d483816e42cd2e01621
Oleksandr Zinkevych <zso040399@gmail.com>
2018-08-29 14:46:15 +0300

    Added check for TESTING config variable

--- griphook/server/__init__.py
+++ griphook/server/__init__.py
@@ -1,10 +1,9 @@
-
-
 import os
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, verify_jwt_in_request
 
@@ -29,63 +28,55 @@ def create_app(script_info=None):
     migrate.init_app(app, db)
     jwt.init_app(app)
     bcrypt.init_app(app)
-    # register blueprints
-    from griphook.server.billing import billing_blueprint
 
+    # import blueprints
+    from griphook.server.billing import billing_blueprint
+    from griphook.server.settings import settings_blueprint
+    from griphook.server.settings.project import settings_project_blueprint
+    from griphook.server.settings.team import settings_team_blueprint
+    from griphook.server.settings.server import settings_server_blueprint
+    from griphook.server.settings.cluster import settings_cluster_blueprint
+    from griphook.server.auth import auth_blueprint
+    from griphook.server.average_load import average_load_blueprint
+    from griphook.server.peaks import peaks_blueprint
+    from griphook.server.filters import filters_blueprint
+
+    # Prevent tests to fail with wrong status code assertion
+    if app.config.get("TESTING"):
+
+        # PROTECT SETTINGS FROM UNAUTHORIZED USERS
+        settings_blueprint.before_request(verify_jwt_in_request)
+        settings_project_blueprint.before_request(verify_jwt_in_request)
+        settings_team_blueprint.before_request(verify_jwt_in_request)
+        settings_server_blueprint.before_request(verify_jwt_in_request)
+        settings_cluster_blueprint.before_request(verify_jwt_in_request)
+
+    # Billing
     app.register_blueprint(billing_blueprint, url_prefix="/billing")
 
-    ##########################################################################
-    # PROTECTED FROM UNAUTHORIZED USERS                                      #
-    ##########################################################################
-    from griphook.server.settings import settings_blueprint
-
-    settings_blueprint.before_request(verify_jwt_in_request)
+    # Settings
     app.register_blueprint(settings_blueprint, url_prefix="/settings")
-
-    from griphook.server.settings.project import settings_project_blueprint
-
-    settings_project_blueprint.before_request(verify_jwt_in_request)
     app.register_blueprint(
         settings_project_blueprint, url_prefix="/settings/project"
     )
-
-    from griphook.server.settings.team import settings_team_blueprint
-
-    settings_team_blueprint.before_request(verify_jwt_in_request)
     app.register_blueprint(settings_team_blueprint, url_prefix="/settings/team")
-
-    from griphook.server.settings.server import settings_server_blueprint
-
-    settings_server_blueprint.before_request(verify_jwt_in_request)
     app.register_blueprint(
         settings_server_blueprint, url_prefix="/settings/server"
     )
-
-    from griphook.server.settings.cluster import settings_cluster_blueprint
-
-    settings_cluster_blueprint.before_request(verify_jwt_in_request)
     app.register_blueprint(
         settings_cluster_blueprint, url_prefix="/settings/cluster"
     )
 
-    ##########################################################################
-    # END PROTECTION                                                         #
-    ##########################################################################
-
-    from griphook.server.filters import filters_blueprint
-
+    # Filters
     app.register_blueprint(filters_blueprint, url_prefix="/filters")
 
-    from griphook.server.peaks import peaks_blueprint
-
+    # Peaks
     app.register_blueprint(peaks_blueprint, url_prefix="/peaks")
 
-    from griphook.server.average_load import average_load_blueprint
-
+    # Average load
     app.register_blueprint(average_load_blueprint, url_prefix="/average_load")
 
-    from griphook.server.auth import auth_blueprint
-
+    # Auth
     app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
     # shell context for flask cli
