8a9b5b1aa001bccc6e86cc632e02723dd3442841
Sergiy Sych <sychsergiy2@gmail.com>
2018-08-30 15:11:37 +0300

    billing relative pie chart show percentage labels

--- griphook/frontend/src/billing/pieChart/components/relativeChart.js
+++ griphook/frontend/src/billing/pieChart/components/relativeChart.js
@@ -5,6 +5,21 @@ const RelativePieChartComponent = props => {
   const options = {
     legend: {
       display: false
+    },
+    tooltips: {
+      callbacks: {
+        label: function(tooltipItem, data) {
+          let dataset = data.datasets[tooltipItem.datasetIndex];
+          let total = dataset.data.reduce(
+            (previousValue, currentValue, currentIndex, array) =>
+              previousValue + currentValue
+          );
+          let currentValue = dataset.data[tooltipItem.index];
+          let percentage = Math.floor((currentValue / total) * 100 + 0.5);
+          let currentLabel = data.labels[tooltipItem.index];
+          return currentLabel + ": " + percentage + "%";
+        }
+      }
     }
   };
   return <Pie data={props.chartData} options={options} />;
