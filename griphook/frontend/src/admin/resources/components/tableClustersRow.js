4eeda0609f1d127c6ef65ae0e817de4ec82e0932
d.pavlushchenko <pavlushchenkod@gmail.com>
2018-08-30 18:27:52 +0300

    added cluster_title column to servers table (resources page)

--- griphook/frontend/src/admin/resources/components/tableClustersRow.js
+++ griphook/frontend/src/admin/resources/components/tableClustersRow.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-import InputBoxContainer from "../containers/InputBox"
-
+import InputBoxContainer from "../containers/InputBox";
 
 export const TableClustersRowComponent = props => {
   return (
     <tr>
-        <td>{props.clusterTitle}</td>
-        <InputBoxContainer value={props.clusterCPUPrice} objectId={props.clusterId}  updateObject={props.clusterUpdateCPUPrice} />
-        <InputBoxContainer value={props.clusterMemoryPrice} objectId={props.clusterId}  updateObject={props.clusterUpdateMemoryPrice} />
+      <td>{props.clusterTitle}</td>
+      <InputBoxContainer
+        value={props.clusterCPUPrice}
+        objectId={props.clusterId}
+        updateObject={props.clusterUpdateCPUPrice}
+      />
+      <InputBoxContainer
+        value={props.clusterMemoryPrice}
+        objectId={props.clusterId}
+        updateObject={props.clusterUpdateMemoryPrice}
+      />
     </tr>
   );
 };
