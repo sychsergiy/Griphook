491a4d105c1180202afe9eeb8b51a84dfa9c3817
Oleksandr Zinkevych <zso040399@gmail.com>
2018-08-31 11:53:39 +0300

    added navbar-expand to prevent collapse

--- griphook/frontend/src/common/headerComponent.js
+++ griphook/frontend/src/common/headerComponent.js
@@ -1,56 +1,35 @@
 import React, { Component } from "react";
 
-import {Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const HeaderComponent = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav className="navbar navbar-dark navbar-expand bg-primary">
     <Link to="/" className="navbar-brand">
       <i className="fas fa-coins mr-2" />
       Griphook
     </Link>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button>
-
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <Link to="/billing" className="nav-link">
-            Billing
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <Link to="/billing" className="nav-link">
+          Billing
           </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/peaks" className="nav-link">
-            Peaks
+      </li>
+      <li className="nav-item">
+        <Link to="/peaks" className="nav-link">
+          Peaks
           </Link>
-        </li>
-      </ul>
-      <ul className="navbar-nav ml-auto">
-         <li className="nav-item">
-            <Link to="/settings/tasks" className="nav-link">
-                <i className="fas fa-wrench mr-1" />
-                Settings
+      </li>
+    </ul>
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <Link to="/settings/tasks" className="nav-link">
+          <i className="fas fa-wrench mr-1" />
+          Settings
             </Link>
-         </li>
-      </ul>
-    </div>
+      </li>
+    </ul>
   </nav>
 );
 
 export default HeaderComponent;
 
-
-
-
-        // {/*<li className="nav-item active">*/}
-
-
-        // {/*</li>*/}
