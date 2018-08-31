9a63f3754de6e2c4a5d598f62e054845856004e3
Sergiy Sych <sychsergiy2@gmail.com>
2018-08-31 12:01:56 +0300

    pagination fixed

--- griphook/server/config.py
+++ griphook/server/config.py
@@ -22,7 +22,7 @@ class BaseConfig(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = False
     TESTING = False
-    BILLING_TABLE_METRICS_PER_PAGE = 5
+    BILLING_TABLE_METRICS_PER_PAGE = 10
 
 
 class DevelopmentConfig(BaseConfig):
